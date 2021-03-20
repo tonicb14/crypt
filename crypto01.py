b2dc23762408645efa15abcad2924aa2da151869
Toni C.B <45513544+tonicb14@users.noreply.github.com>
2021-03-20 16:47:47 +0100

    Update crypto01.py

--- crypto01.py
+++ crypto01.py
@@ -1,12 +1,18 @@
 import os
+
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
+# CIFRADO CON AES Y CBC
 key = bytes(str('12345678901234567890123456789012'), 'ascii')
 iv = os.urandom(16)
 cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
 encryptor = cipher.encryptor()
-ct = encryptor.update(b'a secret message') + encryptor.finalize()
+ct = encryptor.update(b"a secret message") + encryptor.finalize()
+# DESCIFRADO
 decryptor = cipher.decryptor()
 dct = decryptor.update(ct) + decryptor.finalize()
+
+# MOSTRAMOS MENSAJE POR PANTALLA
 print(f'Mensaje encryptado:',ct)
-print(f'Mensaje Des-encryptado:',dct)
+print(f'Mensaje encryptado hexadecimal:',ct.hex())
+print(f'Mensaje Des-encryptado:',dct.decode('utf-8'))
